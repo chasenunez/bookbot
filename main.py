8d9adafb44020de79281346afdc1658ec7ced5ae
chasenunez <chasenunez@gmail.com>
2025-10-08 13:36:52 +0200

    working word counter and letter counter. not yet sorted

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from stats import get_book_text
+from stats import count_words
+from stats import count_letters 
+
 def main(file = "books/frankenstein.txt"):
 	text = get_book_text(file)
 	msg = count_letters(text)
-	print(msg)
+	letters = count_letters(text)
+	print (msg)
+	print (letters)
 
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,14 @@ def get_book_text(path_to_file):
                 file_contents = f.read()
         return(file_contents)
 
-def count_letters(file_contents):
+def count_words(file_contents):
         num_words = len(file_contents.split())
         msg = f"Found {num_words} total words"
         return msg
+
+def count_letters(file_contents):
+	small_letters = list(file_contents.lower())
+	letters = dict()
+	for letter in small_letters:
+  		letters[letter] = letters.get(letter, 0) + 1
+	return(letters)
