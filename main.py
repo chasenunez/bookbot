c67ec02c61c9820ae430359c7bd13d1da6b69a4f
chasenunez <chasenunez@gmail.com>
2025-10-08 15:03:29 +0200

    bookbot can sucessfully ingest a book, run stats and print a report: ============ BOOKBOT ============ Analyzing book found at books/frankenstein.txt... ----------- Word Count ---------- Found 75767 total words --------- Character Count ------- e: 44538 t: 29493 a: 25894 o: 24494 i: 23927 n: 23643 s: 20360 r: 20079 h: 19176 d: 16318 l: 12306 m: 10206 u: 10111 c: 9011 f: 8451 y: 7756 w: 7450 p: 5952 g: 5795 b: 4868 v: 3737 k: 1661 x: 691 j: 497 q: 325 z: 235 æ: 28 â: 8 ê: 7 ë: 2 ô: 1 ============= END ===============

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from stats import get_book_text
 from stats import count_words
 from stats import count_letters 
+from stats import order
 
 def main(file = "books/frankenstein.txt"):
 	text = get_book_text(file)
-	msg = count_letters(text)
+	words = count_words(text)
 	letters = count_letters(text)
-	print (msg)
-	print (letters)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {file}...")
+	print("----------- Word Count ----------")
+	print(f"{words}")
+	print("--------- Character Count -------")
+	ordered_list = order(letters)
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def get_book_text(path_to_file):
         with open(path_to_file) as f:
                 file_contents = f.read()
-        return(file_contents)
+        return file_contents
 
 def count_words(file_contents):
         num_words = len(file_contents.split())
@@ -10,7 +10,16 @@ def count_words(file_contents):
 
 def count_letters(file_contents):
 	small_letters = list(file_contents.lower())
-	letters = dict()
+	letter_counts = dict()
 	for letter in small_letters:
-  		letters[letter] = letters.get(letter, 0) + 1
-	return(letters)
+		if letter.isalpha():
+			letter_counts[letter] = letter_counts.get(letter, 0) + 1
+		else:
+			continue
+
+	return letter_counts
+
+def order(letter_counts):
+	ordered_letters = dict(sorted(letter_counts.items(), key=lambda item:item[1], reverse = True))
+	for entry in ordered_letters:
+                print(f"{entry}: {ordered_letters[entry]}")
